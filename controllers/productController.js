dc650eaadb4c7080b7abac361e374f55a8dbdc2c
kasahun-welela <kasahunwelela1@gmail.com>
2026-01-12 17:11:46 +0300

    remove

--- controllers/productController.js
+++
@@ -1,6 +0,0 @@
-export const getAllProducts = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "List of produts",
-  });
-};
--- routes/productRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {getAllProducts} from "../controllers/productController.js";
-
-const router =Router()
-
-router.get("/products",getAllProducts)
-
-export default router

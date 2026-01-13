7609cc845087c59e016f47b64f6925cccf5b6bcd
kasahun-welela <kasahunwelela1@gmail.com>
2026-01-13 15:54:57 +0300

    get product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,37 @@
 import Product from "../model/product.js";
-export const getAllProducts = (req, res) => {
+export const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  if (!products || products.length === 0)
+    return res.status(404).json({
+      success: false,
+      description: "No products found",
+    });
   res.status(200).json({
     success: true,
-    message: "List of produts",
+    count: products.length,
+    products,
   });
 };
-export const createProduct= async(req,res)=>{
-  const product =await Product.create(req.body)
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
 
   res.status(201).json({
-    success:true,
-    product
-  })
-}
+    success: true,
+    product,
+  });
+};
+
+export const getSingleProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product)
+    return res.status(404).json({
+      success: false,
+      description: "Product not found",
+    });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {getAllProducts,createProduct} from "../controllers/productController.js";
+import {getAllProducts,createProduct,getSingleProduct} from "../controllers/productController.js";
 
 const router =Router()
 
 router.get("/products",getAllProducts)
 router.post("/product/new",createProduct)
+router.get("/product/:id",getSingleProduct)
 export default router

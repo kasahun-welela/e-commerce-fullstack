5ff58f0f71868ff9437d68854c101157b21059be
kasahun-welela <kasahunwelela1@gmail.com>
2026-01-15 19:33:41 +0300

    handle uncaught exceptions

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,22 @@ import dotenv from "dotenv"
 import app from "./app.js"
 import ConnectDB from "./config/db.js"
 
+//handle uncaught exception
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err.message);
+  process.exit(1);
+});
+
 dotenv.config()
 const PORT = process.env.PORT
 ConnectDB()
+console.log(a)
 
 app.listen(PORT, ()=>{
     console.log(`the server started on ${PORT}`)
 })
 
+//handle unhandled promise rejection
 process.on("unhandledRejection", (err) => {
   console.error("Unhandled Rejection:", err.message);
   process.exit(1);

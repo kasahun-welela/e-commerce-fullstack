602ffd38229d8867f782efde2a5cff30801ce100
kasahun-welela <kasahunwelela1@gmail.com>
2026-01-23 22:55:42 +0300

    add authentication routes and user registration logic

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,10 @@
-import express from "express"
-import produts from "./routes/productRoute.js"
+import express from "express";
+import products from "./routes/productRoute.js";
+import auth from "./routes/authRoute.js";
 
+const app = express();
+app.use(express.json());
 
-
-const app=express()
-app.use(express.json())
-
-app.use("/api/v1",produts)
-
-
-
-
-
+app.use("/api/v1", products);
+app.use("/api/v1", auth);
 export default app;
---
+++ backend/controllers/authController.js
@@ -0,0 +1,25 @@
+import { User } from "../model/user.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+      avatar: {
+        public_id: "sample_id",
+        url: "sample_url",
+      },
+    });
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};

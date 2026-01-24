a848cc4934d544ba38478a2af112d78a466296a0
kasahun-welela <kasahunwelela1@gmail.com>
2026-01-24 22:36:58 +0300

    add login functionality and password comparison method

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,8 +12,43 @@ export const registerUser = async (req, res) => {
         url: "sample_url",
       },
     });
+    user.password = undefined;
     res.status(201).json({
       success: true,
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    user.password = undefined;
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
       user,
     });
   } catch (err) {
--- backend/model/user.js
+++ backend/model/user.js
@@ -47,4 +47,8 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 export const User = mongoose.model("User", userSchema);
